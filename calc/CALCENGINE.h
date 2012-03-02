10a31b995c039397cc92b9a9271af0719150efa4
math4tots <math4tots@gmail.com>
2012-03-01 16:32:09 -0800

    changed name of eng.h to CALCENGINE.h

---
+++ calc/CALCENGINE.h
@@ -0,0 +1,11 @@
+/*
+Calculator engine
+
+Single C header file.
+
+I want to keep the calculator engine extremely small and portable.
+
+So I may break some conventionally good programming (e.g. avoiding goto's).
+*/
+
+#ifdef CALCENGINE_H
